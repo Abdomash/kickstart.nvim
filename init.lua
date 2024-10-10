5940677815315ffb733ca3881963a6f189dd42ef
Abdomash <abdomash2010@gmail.com>
2024-10-09 19:01:12 -0500

    Add lsp and comments

--- init.lua
+++ init.lua
@@ -244,8 +244,9 @@ require('lazy').setup({
   -- Use `opts = {}` to force a plugin to be loaded.
   --
 
-  -- Custom colorscheme
-  'Mofiqul/dracula.nvim',
+  -- Custom colorschemes
+  'dracula/vim',
+  'catppuccin/nvim',
 
   -- File Explorer
   {
@@ -258,6 +259,12 @@ require('lazy').setup({
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
   },
+
+  -- Toggle Comments
+  'tpope/vim-commentary',
+
+  -- Copilot
+  'github/copilot.vim',
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
@@ -679,8 +686,11 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'pylint',
+        -- 'pylint',
+        -- 'python-lsp-server',
+        'pyright',
         'typescript-language-server',
+        'tailwindcss-language-server',
         'prettier',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
@@ -864,13 +874,13 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'dracula'
       -- vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
