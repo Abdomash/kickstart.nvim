a39ba1c715954737919164ffdca68a223e205a24
Abdomash <abdomash2010@gmail.com>
2024-10-17 18:00:08 -0500

    Add transparent bg

--- lua/abdo/colors.lua
+++ lua/abdo/colors.lua
@@ -5,16 +5,17 @@ return {
   {
     'Mofiqul/dracula.nvim',
     config = function()
-      --   local dracula = require 'dracula'
-      --   dracula.setup {
-      --     colors = {
-      --       bg = '#000000',
-      --       visual = '#1b1a2b',
-      --       menu = '#1b1a2b',
-      --       black = '#1b1a2b',
-      --       selection = '#1b1a2b',
-      --     },
-      --   }
+      local dracula = require 'dracula'
+      dracula.setup {
+        transparent_bg = true,
+        -- colors = {
+        --   bg = '#000000',
+        --   visual = '#1b1a2b',
+        --   menu = '#1b1a2b',
+        --   black = '#1b1a2b',
+        --   selection = '#1b1a2b',
+        -- },
+      }
     end,
   },
 
